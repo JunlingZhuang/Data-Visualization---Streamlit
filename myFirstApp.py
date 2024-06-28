7152ca6b80cd2d395bd1ea81b1a94322f2a57b63
ZHUANG JUNLING <108850976+JunlingZhuang@users.noreply.github.com>
2024-06-28 14:12:52 -0400

    update first App

--- myFirstApp.py
+++ myFirstApp.py
@@ -4,10 +4,24 @@ Here's our first attempt at using data to create a table:
 """
 
 import streamlit as st
+import numpy as np
 import pandas as pd
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
 
-df
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+
+x = st.slider('x')  # 👈 this is a widget
+st.write(x, 'squared is', x * x)
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+    })
+
+option = st.selectbox(
+    'Which number do you like best?',
+     df['first column'])
+
+'You selected: ', option
